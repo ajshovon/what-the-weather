f66e77b119c2caca27a2e0cbdb0ced746fc05cfe
Jubayer <jubayeras382@outlook.com>
2024-10-31 14:46:24 +0600

    add animation icons
    Signed-off-by: Jubayer <jubayeras382@outlook.com>

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,16 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { handle } from "hono/vercel";
+import { env } from 'hono/adapter'
 
 const app = new Hono().basePath("/api");;
 app.use('/*', cors());
 
-const WEATHER_API_KEY = '2c34f3ac415c611be0e3f6b9cf55e5f6'; // Replace with your OpenWeatherMap API key
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 app.get('/weather/:lat/:lon', async (c) => {
   const { lat, lon } = c.req.param();
+  const { WEATHER_API_KEY } = env<{ WEATHER_API_KEY: string }>(c)
   const response = await fetch(
     `${BASE_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
   );
@@ -19,6 +20,7 @@ app.get('/weather/:lat/:lon', async (c) => {
 
 app.get('/location/:query', async (c) => {
   const { query } = c.req.param();
+  const { WEATHER_API_KEY } = env<{ WEATHER_API_KEY: string }>(c)
   const response = await fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${WEATHER_API_KEY}`
   );
